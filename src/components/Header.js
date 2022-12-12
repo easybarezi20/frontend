c311c3f1c82962853e85326deb251e1c33d601d3
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2022-12-12 13:52:49 -0800

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-function Header() {
+function Header(props) {
+  //inline style for the nav tag
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    border: "3px solid black",
+    padding: "8px",
+    width: "90%",
+    margin: "auto",
+  };
+
   return (
-    <div>Header</div>
-  )
+    <header>
+      <h1>My Portfolio Page</h1>
+      <nav style={navStyle}>
+        <Link to="/">
+          <div>HOME</div>
+        </Link>
+        <Link to="/about">
+          <div>ABOUT</div>
+        </Link>
+        <Link to="/projects">
+          <div>PROJECTS</div>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
